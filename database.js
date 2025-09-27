4b5b741658791241fb42abf3d54f43aeaade32ce
Maverick2506 <sisodi.aditya83@gmail.com>
2025-09-27 01:19:51 -0400

    Transfer 1

--- database.js
+++ database.js
@@ -1,7 +1,16 @@
 const { Sequelize } = require("sequelize");
 
+// The DATABASE_URL environment variable will be in the format:
+// mysql://user:password@host:port/database
+// or
+// postgres://user:password@host:port/database
+console.log(
+  "DATABASE_URL variable from environment:",
+  process.env.DATABASE_URL
+);
+
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialect: "mysql",
+  dialect: process.env.DB_DIALECT || "postgres",
   dialectOptions: {
     ssl: {
       require: true,
